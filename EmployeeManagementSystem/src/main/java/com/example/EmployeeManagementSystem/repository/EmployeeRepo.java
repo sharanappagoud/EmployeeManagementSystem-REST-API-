8e86132b2e3ef3c28a91e8895e99580fb68fde04
sharanappagouda <sharanappagouda148@gmail.com>
2026-08-18 13:13:27 +0530

    Delete EmployeeManagementSystem directory

--- EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.EmployeeManagementSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.EmployeeManagementSystem.entity.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, String> {
-
-}
